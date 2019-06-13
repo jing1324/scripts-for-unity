48096385db73a911d43cf0ec09f1791085987550
Jin Jing <jinjing123@gmail.com>
2019-06-13 10:48:49 +0800

    Add quiz script

---
+++ Quiz/Option.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//attach this script to the buttons as options
+public class Option: MonoBehaviour
+{ 
+    public Text displayedText;
+    public bool isCorrect;
+    public Quiz parent;
+    //this field is the quiz that the option belongs to
+
+
+    // public Option(string displayedText, bool isCorrect) {
+    //     this.displayedText=displayedText;
+    //     this.isCorrect=isCorrect;
+    // } 
+   void Update(){} 
+    public void changeButtonColor() {
+           ColorBlock colors= GetComponent<Button> ().colors;
+           if(isCorrect) {
+           colors.selectedColor= Color.green;
+           colors.normalColor= Color.green;
+           colors.highlightedColor= Color.green;
+           colors.pressedColor = Color.green;
+           colors.disabledColor = Color.green;
+           parent.increaseScore();
+
+           } else {
+               colors.selectedColor= Color.red;
+               colors.normalColor = Color.red;
+               colors.highlightedColor = Color.red;
+               colors.pressedColor = Color.red;
+               colors.disabledColor = Color.red;
+           }
+           GetComponent<Button>().colors = colors;
+           setUninteractable();
+           parent.increaseClicks();
+
+    }
+    public void resetColors(){
+        ColorBlock colors = GetComponent<Button> ().colors;
+        colors.selectedColor = Color.white;
+        colors.normalColor = Color.white;
+        colors.highlightedColor = Color.white;
+        colors.pressedColor = Color.white;
+        colors.disabledColor = Color.white;
+        GetComponent<Button>().colors = colors;
+    }
+   public void resetText(){
+       displayedText.text = null;
+   } 
+   public void resetAll(){
+       resetColors();
+       resetText();
+       setUninteractable();
+   }
+   public void setUninteractable(){
+    GetComponent<Button>().interactable = false;
+   }
+   public void setInteractable(){
+       GetComponent<Button>().interactable = true;
+   }
+}
---
+++ Quiz/Quiz.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Quiz : MonoBehaviour
+{
+    [System.Serializable]
+    public class Question
+    {
+        [System.Serializable]
+        public class Answer
+        {
+            public bool isCorrect;
+            public string text;
+        }
+
+        public string question;
+        public Answer[] options;
+        // The number of options the question has.
+
+    }
+    public Question[] Questions;
+    //size of this is the number of questions
+    private Question currentQuestion;
+    private int questionIndex;
+    private int scoreCount;
+    private const int maxClicks = 2;
+    private int currentClicks;
+
+    public Option[] Options;
+    // The size for options is the maximum number of options for the question
+    public Text displayText;
+    // Start is called before the first frame update
+    void Start(){
+        questionIndex = -1;
+        scoreCount = 0;
+        nextQuestion();
+    }
+    private void nextQuestion(){
+        currentClicks = 0;
+        questionIndex++;
+        if(questionIndex >= Questions.Length) {
+            endQuiz();
+        } else{
+        currentQuestion = Questions[questionIndex];
+        displayText.text = currentQuestion.question;
+        for(int i=0 ; i < Options.Length; i++) {
+            setOption(Options[i],currentQuestion.options[i]);            
+        }
+        resetOptionColors();
+        }
+    }
+
+    void setOption(Option opt, Question.Answer questionOption){
+        opt.isCorrect = questionOption.isCorrect;
+        opt.displayedText.text = questionOption.text;
+    }
+    void resetOptionColors(){
+        foreach(Option option in Options){
+            option.resetColors();
+        }
+    }
+    void endQuiz(){
+        foreach(Option option in Options){
+            option.resetAll();
+        }
+        displayText.text = "Your score is " + scoreCount.ToString() + "/"+ totalScore().ToString() + ".";
+        GameObject.Find("nextbutton").GetComponentInChildren<Text>().text = "Click to exit";
+    }
+    public void WaitForSecondsNextQuestion() {
+
+        StartCoroutine(nextQuestionWithWait());
+    }
+    public void increaseClicks(){
+        currentClicks++;
+        if(currentClicks == maxClicks) {
+      
+            WaitForSecondsNextQuestion();
+        }
+    }
+    public void increaseScore(){
+        scoreCount++;
+    }
+    IEnumerator nextQuestionWithWait()
+    {     
+        foreach(Option option in Options){
+            option.setUninteractable();
+        }
+        yield return new WaitForSeconds(1);
+        nextQuestion(); 
+        if(questionIndex < Questions.Length){ 
+        foreach(Option option in Options){
+            option.setInteractable();
+        }
+        }
+    }
+    private int totalScore(){
+        return Questions.Length * 2;
+    }
+
+}
